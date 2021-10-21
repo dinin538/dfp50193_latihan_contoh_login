9625ebf04fff5adb527463b4b3b6980f62f292c3
dinin538 <dnnblh@gmail.com>
2021-10-21 09:50:38 +0800

    cnth

---
+++ conn.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+$host ='localhost';
+$user='root';
+$pswd='';
+$dbname='contoh_login';
+
+$conn=new mysqli($host,$user,$pswd,$dbname);
+session_start(); 
+    ?>
+</body>
+</html>
